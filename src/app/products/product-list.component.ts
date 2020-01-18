447682f6b203ce41b53503813b05b14b16cc141e
Shawn T. Dougherty <Shawn.Dougherty.NJ@gmail.com>
2020-01-18 18:25:04 -0500

    Added Observable to product service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -21,41 +21,48 @@ implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage = '';
 
-  _listFilter: string;
+  _listFilter = '';
   get listFilter(): string{
       return this._listFilter;
   }
   set listFilter(value:string){
       this._listFilter = value;
       this.filteredProducts = 
-        this.listFilter ? this.performFilter(this.listFilter) : this.products;
+      this.listFilter ? this.performFilter(this.listFilter) : this.products;
   }
 
-  filteredProducts: IProduct[];
+  filteredProducts: IProduct[] = [];
   products: IProduct[] = [];
-
-  ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
-  }
-
+  
   constructor(private productService: ProductService)  {
   }
 
+  
   onRatingClicked(message: string): void {
-      this.pageTitle = 'Product List: ' + message;
+    this.pageTitle = 'Product List: ' + message;
   }
-
+  
   performFilter(filterBy: string): IProduct[] {
-      filterBy = filterBy.toLocaleLowerCase();
-      return this.products.filter((product: IProduct) =>
-            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+    product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
-
+  
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
-
+  
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products
+        this.filteredProducts = this.products;
+      },
+      error(err) {this.errorMessage = err}
+    });
+    
+  }
   
 }
