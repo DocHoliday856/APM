db5339dfa035f7e55319c1169b72d0f09f256de4
Shawn Dougherty <sdougherty@milestechnologies.com>
2020-01-21 19:47:22 -0500

    Created and imported shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StarComponent } from './star.component';
+import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './convertToSpaces.pipe';
+
+
+
+@NgModule({
+  declarations: [
+    StarComponent,
+    ConvertToSpacesPipe
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    StarComponent,
+    FormsModule,
+    CommonModule,
+    ConvertToSpacesPipe
+  ]
+})
+export class SharedModule { }
